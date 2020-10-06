cc8bd64a52645076f72ffd7b22f579fb81405be5
William <capuletos@live.com>
2020-10-06 18:01:14 -0300

    Criando elemento no fim da lista

---
+++ No.cs
@@ -0,0 +1,8 @@
+﻿namespace ChainedList
+{
+    internal class No
+    {
+        internal string Value;
+        internal No Next;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainedList
+{
+    class List
+    {
+        private No _glass, _water;
+
+        //Inserir elemento no fim da lista
+        public void InsertEnd(string element)
+        {
+            No newNo = new No();
+            newNo.Value = element;
+
+            if (_glass == null)
+            {
+                _glass = newNo;
+            }
+            else
+            {
+                _water.Next = newNo;
+            }
+        }
+
+
+    }
+}
